48c5d3279a7c4a123d9b9f8788f0850bdb0e6e2d
siko0001 <tom.sikora@etudiant.univ-reims.fr>
2024-12-03 15:10:10 +0100

    feat: use button card component

--- src/components/MainMenu.jsx
+++ src/components/MainMenu.jsx
@@ -1,17 +1,32 @@
 import React from "react";
+import { ClockCircleOutlined, ReloadOutlined, UserOutlined } from "@ant-design/icons";
+import ButtonCard from "./ButtonCard";
 
 export default function MainMenu() {
     return (
-        <div className="flex flex-col gap-4">
-            <a href="/main-game" aria-label="main-game">
-                <div className="">
-                    <i className="bx bx-list-ul" />
-                </div>
-                <div className="">
-                    <p>Journalier</p>
-                    <p>Devine la personne du jour.</p>
-                </div>
-            </a>
+        <div className="flex flex-col gap-4 mt-16">
+            <ButtonCard
+                link="/main-game"
+                icon={
+                    <ClockCircleOutlined className="text-5xl text-light-blue-but" />
+                }
+                title="Daily"
+                description="Devine la personne du jour."
+            />
+            <ButtonCard
+                link="/"
+                icon={
+                    <ReloadOutlined className="text-5xl text-light-blue-but" />
+                }
+                title="Unlimited"
+                description="Joue sans aucune limite."
+            />
+            <ButtonCard
+                link="/"
+                icon={<UserOutlined className="text-5xl text-light-blue-but" />}
+                title="The Face"
+                description="Trouve avec l'avatar."
+            />
         </div>
     );
 }
