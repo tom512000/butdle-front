d8eb32f886a09fa3725bf7792f1ad8165d361c19
Tom Sikora <tom.sikora@etudiant.univ-reims.fr>
2024-12-06 11:16:28 +0100

    feat: add tutorial button

--- src/components/UnlimitedMainGame.jsx
+++ src/components/UnlimitedMainGame.jsx
@@ -3,6 +3,7 @@ import {
     CloseOutlined,
     SearchOutlined,
     ReloadOutlined,
+    QuestionOutlined,
 } from "@ant-design/icons";
 import data from "../data/persons.json";
 import PersonCard from "./PersonCard";
@@ -152,28 +153,36 @@ export default function UnlimitedMainGame() {
                 </div>
             ) : (
                 <div className="relative w-[320px] mt-10">
-                    <div className="relative mb-0.5">
-                        <input
-                            type="text"
-                            value={input}
-                            onChange={handleInputChange}
-                            placeholder="Entrez un nom ou un prénom"
-                            aria-label="search"
-                            className="appearance-none border-2 pl-10 bg-gray-800 border-gray-800 hover:border-gray-600 transition-colors rounded-md w-full py-2 px-3 font-inter text-sm text-gray-300 leading-tight focus:outline-none"
-                        />
+                    <div className="relative flex">
                         <button
                             type="button"
-                            onClick={handleClearInput}
-                            aria-label="cross"
-                            className="absolute right-0 inset-y-0 flex items-center cursor-pointer"
+                            className="w-[37px] min-w-[37px] h-[37px] flex justify-center items-center mr-3 group bg-gray-800 rounded-md"
                         >
-                            <CloseOutlined className="bx bx-x text-xl ml-3 mr-3 text-gray-500 hover:text-gray-600" />
+                            <QuestionOutlined className="text-xl text-gray-500 group-hover:text-gray-600 transition-colors duration-200" />
                         </button>
-                        <div
-                            aria-label="search"
-                            className="absolute left-0 inset-y-0 flex items-center"
-                        >
-                            <SearchOutlined className="bx bx-search text-xl mx-3 text-gray-500" />
+                        <div className="relative h-[37px] mb-0.5">
+                            <input
+                                type="text"
+                                value={input}
+                                onChange={handleInputChange}
+                                placeholder="Entrez un nom ou un prénom"
+                                aria-label="search"
+                                className="appearance-none border-2 pl-10 bg-gray-800 border-gray-800 hover:border-gray-600 transition-colors rounded-md w-[300px] py-2 px-3 font-inter text-sm text-gray-300 leading-tight focus:outline-none"
+                            />
+                            <button
+                                type="button"
+                                onClick={handleClearInput}
+                                aria-label="cross"
+                                className="absolute right-0 inset-y-0 flex items-center cursor-pointer"
+                            >
+                                <CloseOutlined className="text-xl ml-3 mr-3 text-gray-500 hover:text-gray-600" />
+                            </button>
+                            <div
+                                aria-label="search"
+                                className="absolute left-0 inset-y-0 flex items-center"
+                            >
+                                <SearchOutlined className="text-xl mx-3 text-gray-500" />
+                            </div>
                         </div>
                     </div>
                     <div
@@ -183,7 +192,7 @@ export default function UnlimitedMainGame() {
                                 suggestions.length > 0
                                     ? "1px solid #444852"
                                     : "none",
-                            boxShadow: "0px 4px 6px rgba(0, 0, 0, 0.1)",
+                            boxShadow: "0px 4px 6px rgba(0, 0, 0, 0.1)"
                         }}
                     >
                         {suggestions.map((person) => (
